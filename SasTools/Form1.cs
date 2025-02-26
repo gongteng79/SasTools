c1b598eb9c03ca8480b26765e94f35afba00ae5a
gongteng79 <1240577870@qq.com>
2025-02-27 01:11:24 +0800

    close 6#

--- SasTools/Form1.cs
+++ SasTools/Form1.cs
@@ -16,5 +16,10 @@ namespace SasTools
         {
             InitializeComponent();
         }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello World!");
+        }
     }
 }
