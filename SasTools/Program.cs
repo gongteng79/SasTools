776b7e7d132dec089247310ad59120cf3e731940
Levi <96852740+gongteng79@users.noreply.github.com>
2025-02-27 23:36:06 +0800

    close #1 (#8)

--- SasTools/Program.cs
+++ SasTools/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WpFramework.LogFactory;
 
 namespace SasTools
 {
@@ -16,7 +17,8 @@ namespace SasTools
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Log.Configure();
+            Application.Run(new OverView());
         }
     }
 }
---
+++ SasTools/UI/OverView.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AntdUI;
+using WpFramework.LogFactory;
+
+namespace SasTools
+{
+    public partial class OverView: Window
+    {
+        public OverView()
+        {
+            InitializeComponent();
+        }
+    }
+}
