be28914b5d915a589d0c8b1ed914cca1566453e7
Levi <96852740+gongteng79@users.noreply.github.com>
2025-04-09 22:29:34 +0800

    新增手动操作界面

---
+++ SasTools/UI/ManualView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SasTools.UI
+{
+    public partial class ManualView: UserControl
+    {
+        public ManualView()
+        {
+            InitializeComponent();
+        }
+    }
+}
